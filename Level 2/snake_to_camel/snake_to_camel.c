f43ae4ac72ce1725460ab29e30f0169de534d07b
Kenny Siwawa Chikwama <kchikwam@student.42wolfsburg.de>
2024-10-09 12:28:40 +0000

    Exam 02

--- Level 2/snake_to_camel/snake_to_camel.c
+++ Level 2/snake_to_camel/snake_to_camel.c
@@ -1,66 +1,23 @@
 #include <unistd.h>
 
-void    ft_putchar(char c)
-{
-    write(1, &c, 1);
-}
-
-int lowerCamelCase(char c)
-{
-
-}
-
 int main(int argc, char **argv)
 {
     int i;
 
     i = 0;
-    if (argc == 0)
+    if (argc == 2)
     {
         while(argv[1][i])
         {
-            if(argv[1][i] == '_')
+            if (argv[1][i] == '_')
             {
-                lowerCamelCase(argv[1][i]);
+                i++;
+                argv[1][i] = argv[1][i] - 32;
             }
-            else
-            {
-                ft_putchar(argv[1][i]);
-            }
+            write(1, &argv[1][i], 1);
+            i++;
         }
+        write(1, "\n", 1);
+        return (0);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// A snake_case string is a string where each word is in lower case, separated by
-// an underscore "_".
-
-// A lowerCamelCase string is a string where each word begins with a capital letter
-// except for the first one.
-
-// Examples:
-// $>./camel_to_snake "here_is_a_snake_case_word"
-// hereIsASnakeCaseWord
-// $>./camel_to_snake "hello_world" | cat -e
-// helloWorld$
-// $>./camel_to_snake | cat -e
-// $
